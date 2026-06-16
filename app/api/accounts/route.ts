ade39ee3e2e8c852258e3e95b757dde261a63c1c
Chetan Shirke <sam808201010@gmail.com>
2026-06-16 09:34:44 +0000

    Accounts API

---
+++ app/api/accounts/route.ts
@@ -0,0 +1,13 @@
+import { prisma } from "@/lib/prisma";
+import { NextResponse } from "next/server";
+
+export async function GET() {
+  const accounts =
+    await prisma.tradingAccount.findMany({
+      orderBy: {
+        createdAt: "desc",
+      },
+    });
+
+  return NextResponse.json(accounts);
+}
